54053820ac1ccf77981a003a561a3013ab3787df
Joanna <joanna.kilar.jk@gmail.com>
2021-04-12 10:28:40 +0200

    1

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+// import { DataGrid } from '@material-ui/data-grid'
+// import * as React from 'react'
+// import axios from 'axios'
 
-const Groups = () => {
-  return (
-    <div>
-      <h1>Grupy</h1>
-    </div>
-  )
-}
+// const columns = [
+//   { field: 'groupName', headerName: 'Nazwa grupy', width: 140 },
+//   { field: 'danceType', headerName: 'Rodzaj tańca', width: 140 },
+//   { field: 'numberOfParticipant', headerName: 'Liczba uczestników', width: 180 },
+//   { field: 'level', headerName: 'Stopień zaawan.', width: 180 },
+//   { field: 'instructor', headerName: 'Instruktor/-rzy', width: 140,
+//     valueGetter: (params) =>
+//       `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
+//   },
+//   { field: 'comment', headerName: 'Komentarz', width: 140 },
+//   { field: 'action', headerName: 'Akcja', width: 100 },
+// ]
 
-export default Groups
+// const rows = [
+//   { id: 1, lastName: 'Snow', firstName: 'Jon'},
+//   { id: 2, lastName: 'Lannister', firstName: 'Cersei'},
+//   { id: 3, lastName: 'Lannister', firstName: 'Jaime'},
+//   { id: 4, lastName: 'Stark', firstName: 'Arya'},
+//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys'},
+//   { id: 6, lastName: 'Melisandre', firstName: null},
+//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara'},
+//   { id: 8, lastName: 'Frances', firstName: 'Rossini'},
+//   { id: 9, lastName: 'Roxie', firstName: 'Harvey'},
+// ]
+
+// const Groups = () => {
+  
+
+//     axios.get('http://localhost:8010/proxy/users',)
+//       .then(response => {
+//         console.log(response.data)
+//       .catch(error => {
+//         console.log('error', error)
+//         })
+//       })
+
+//   return (
+//     <div>
+//       <h1>Grupy</h1>
+//       <div style={{ height: 400, width: '100%' }}>
+//       <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+//       </div>
+//     </div>
+
+//   )
+// }
+
+// export default Groups
+
+
+
+
+
+//`````````````````````````````````````````````````````````````````````````````````````````````
