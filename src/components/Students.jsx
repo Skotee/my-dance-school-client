4b58c93aedab26d6af94b8c660b995947151738c
Adam Spinek <adamspinek@gmail.com>
2021-04-12 20:11:28 +0200

    feat(students): change react table to use material UI

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,287 +1,214 @@
 import React, { useState, useEffect } from 'react'
 
-import styled from 'styled-components'
-import { useTable, useSortBy, useFilters, useColumnOrder } from 'react-table'
-import { motion, AnimatePresence } from 'framer-motion'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import MaUTable from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
 import { matchSorter } from 'match-sorter'
 import ActionsMenu from './ActionsMenu.jsx'
 import axios from 'axios'
 
-
-const Styles = styled.div`
-  padding: 1rem;
-
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-      background: white;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-`
-
-function Filtr() {
-
-  return (
-    <br    />
-  )
-}
+import { useTable, useSortBy, useFilters, useColumnOrder, useBlockLayout } from 'react-table'
 
 
 function DefaultColumnFilter({
-  column: { filterValue, preFilteredRows, setFilter },
-}) {
-  const count = preFilteredRows.length
-
-  return (
-    <input
-      value={filterValue || ''}
-      onChange={e => {
-        setFilter(e.target.value || undefined)
-      }}
-      placeholder={`Przeszukaj ${count} rekordów...`}
-    />
-  )
-}
-
-function SelectColumnFilter({
-  column: { filterValue, setFilter, preFilteredRows, id },
-}) {
-  const options = React.useMemo(() => {
-    const options = new Set()
-    preFilteredRows.forEach(row => {
-      options.add(row.values[id])
-    })
-    return [...options.values()]
-  }, [id, preFilteredRows])
-
-  return (
-    <select
-      value={filterValue}
-      onBlur={e => {
-        setFilter(e.target.value || undefined)
-      }}
-    >
-      <option value="">All</option>
-      {options.map((option, i) => (
-        <option key={i} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
-  )
-}
-
-function fuzzyTextFilterFn(rows, id, filterValue) {
-  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
-}
-
-fuzzyTextFilterFn.autoRemove = val => !val
+    column: { filterValue, preFilteredRows, setFilter },
+  }) {
+    const count = preFilteredRows.length
+  
+    return (
+      <input
+        value={filterValue || ''}
+        onChange={e => {
+          setFilter(e.target.value || undefined)
+        }}
+        placeholder={`Przeszukaj ${count} rekordów...`}
+      />
+    )
+  }
+  
+  function SelectColumnFilter({
+    column: { filterValue, setFilter, preFilteredRows, id },
+  }) {
+    const options = React.useMemo(() => {
+      const options = new Set()
+      preFilteredRows.forEach(row => {
+        options.add(row.values[id])
+      })
+      return [...options.values()]
+    }, [id, preFilteredRows])
+  
+    return (
+      <select
+        value={filterValue}
+        onBlur={e => {
+          setFilter(e.target.value || undefined)
+        }}
+      >
+        <option value="">All</option>
+        {options.map((option, i) => (
+          <option key={i} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    )
+  }
+  
+  function fuzzyTextFilterFn(rows, id, filterValue) {
+    return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+  }
+  
+  fuzzyTextFilterFn.autoRemove = val => !val
 
 function Table({ columns, data }) {
-  const defaultColumn = React.useMemo(
-    () => ({
-      Filter: Filtr,
-    }),
-    []
-  )
 
+    const defaultColumn = React.useMemo(
+        () => ({
+          Filter: DefaultColumnFilter,
+        }),
+        []
+      )
+
+  // Use the state and functions returned from useTable to build your UI
   const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable(
-    {
-      columns,
-      data,
-      defaultColumn,
-    },
-    useColumnOrder,
-    useFilters,
-    useSortBy
-  )
+      getTableProps,
+      headerGroups,
+      rows,
+      prepareRow
+    } = useTable(
+        {
+            columns,
+            data,
+            defaultColumn,
+        },
+        useColumnOrder,
+        useFilters,
+        useSortBy,
+        useBlockLayout
+        )
 
-  const spring = React.useMemo(
-    () => ({
-      type: 'spring',
-      damping: 50,
-      stiffness: 100,
-    }),
-    []
-  )
 
+  // Render the UI for your table
   return (
-    <>
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup, i) => (
-            <tr key={'cell'} {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <motion.th key={column}
-                  {...column.getHeaderProps({
-                    layoutTransition: spring,
-                    style: {
-                      minWidth: column.minWidth,
-                    },
-                  })}
-                >
-                  <div {...column.getSortByToggleProps()}>
-                    {column.render('Header')}
-                    <span>
-                      {column.isSorted
-                        ? column.isSortedDesc
-                          ? ' 🔽'
-                          : ' 🔼'
-                        : ''}
-                    </span>
-                  </div>
-                  <div>{column.canFilter ? column.render('Filter') : null}</div>
-                </motion.th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          <AnimatePresence>
-            {rows.slice(0, 10).map((row, i) => {
-              prepareRow(row)
-              return (
-                <motion.tr key={'row'}
-                  {...row.getRowProps({
-                    layoutTransition: spring,
-                    exit: { opacity: 0, maxHeight: 0 },
-                  })}
-                >
-                  {row.cells.map((cell, i) => {
-                    return (
-                      <motion.td key={'cell'}
-                        {...cell.getCellProps({
-                          layoutTransition: spring,
-                        })}
-                      >
-                        {cell.render('Cell')}
-                      </motion.td>
-                    )
-                  })}
-                </motion.tr>
-              )
-            })}
-          </AnimatePresence>
-        </tbody>
-      </table>
-    </>
+    <MaUTable {...getTableProps()}>
+      <TableHead>
+        {headerGroups.map(headerGroup => (
+          <TableRow key={'row'} {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <TableCell key={'cell'} {...column.getHeaderProps()}>
+                {column.render('Header')}
+                <div>{column.canFilter ? column.render('Filter') : null}</div>
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => {
+          prepareRow(row)
+          return (
+            <TableRow key={'row'} {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return (
+                  <TableCell key={'cell'} {...cell.getCellProps()}>
+                    {cell.render('Cell')}
+                  </TableCell>
+                )
+              })}
+            </TableRow>
+          )
+        })}
+      </TableBody>
+    </MaUTable>
   )
 }
 
-function filterGreaterThan(rows, id, filterValue) {
-  return rows.filter(row => {
-    const rowValue = row.values[id]
-    return rowValue >= filterValue
-  })
-}
-
-filterGreaterThan.autoRemove = val => typeof val !== 'number'
-
-const Students = () => {
-
+function Students() {
   const columns = React.useMemo(
     () => [
       {
         Header: 'Kursanci',
         columns: [
-          {
-            Header: 'Imię',
-            accessor: 'name',
-            minWidth: 150,
-            Filter: DefaultColumnFilter,
-          },
-          {
-            Header: 'Nazwisko',
-            accessor: 'surname',
-            minWidth: 150,
-            filter: 'fuzzyText',
-            Filter: DefaultColumnFilter,
-          },
-          {
-            Header: 'Grupa zajęciowa',
-            accessor: 'group',
-            minWidth: 150,
-            Filter: SelectColumnFilter,
-            filter: 'includes',
-          },
-          {
-            Header: 'Numer telefonu',
-            accessor: 'phoneNumber',
-            minWidth: 150,
-          },
-          {
-            Header: 'Płeć',
-            accessor: 'gender',
-            minWidth: 150,
-          },
-          {
-            Header: 'Zgoda na wysyłanie powiadomień',
-            accessor: 'accept',
-            minWidth: 150,
-          },
-          {
-            Header: 'Akcje',
-            accessor: 'actions',
-            minWidth: 150,
-            Cell: ({ cell }) => <ActionsMenu />
-          },
+            {
+                Header: 'Imię',
+                accessor: 'name',
+                minWidth: 250,
+                Filter: DefaultColumnFilter,
+              },
+              {
+                Header: 'Nazwisko',
+                accessor: 'surname',
+                minWidth: 250,
+                filter: 'fuzzyText',
+                Filter: DefaultColumnFilter,
+              },
+              {
+                Header: 'Grupa zajęciowa',
+                accessor: 'group',
+                minWidth: 150,
+                Filter: SelectColumnFilter,
+                filter: 'includes',
+              },
+              {
+                Header: 'Numer telefonu',
+                accessor: 'phoneNumber',
+                minWidth: 250,
+              },
+              {
+                Header: 'Płeć',
+                accessor: 'gender',
+                minWidth: 150,
+                disableFilters: true,
+              },
+              {
+                Header: 'Zgoda na wysyłanie powiadomień',
+                accessor: 'accept',
+                minWidth: 150,
+                disableFilters: true,
+              },
+              {
+                Header: 'Akcje',
+                accessor: 'actions',
+                minWidth: 150,
+                disableFilters: true,
+                Cell: () => <ActionsMenu />
+              },
         ],
       },
     ],
     []
   )
 
-  const [data, setData] = useState([])
+ const [data, setData] = useState([])
  
-  useEffect(() => {
+ useEffect(() => {
 
-    async function getStudents() {
+   async function getStudents() {
 
-      let people = []
-      await axios.get('http://localhost:3000/users',).then(students => {
-        people = students.data
-        students.data.forEach(async (person, index) => {
-          await axios.get('http://localhost:3000/phones/users/'.concat(person._id.toString()))
-          .then(phone => {
-            people[index].phoneNumber = phone.data[0].phoneNumber
-            setData(people)
-          })
-        })
-      })      
-    }
-      getStudents()
-      
-  }, [])
+     let people = []
+     await axios.get('http://localhost:3000/users',).then(students => {
+       people = students.data
+       students.data.forEach(async (person, index) => {
+         await axios.get('http://localhost:3000/phones/users/'.concat(person._id.toString()))
+         .then(phone => {
+           people[index].phoneNumber = phone.data[0].phoneNumber
+           setData(people)
+         })
+       })
+     })      
+   }
+     getStudents()
+     
+ }, [])
 
   return (
-    <Styles>
+    <div>
+      <CssBaseline />
       <Table columns={columns} data={data} />
-    </Styles>
+    </div>
   )
 }
 
