e6026ed03ec3944a74fda741029a44bb326ebb71
Joanna <joanna.kilar.jk@gmail.com>
2021-04-14 22:36:19 +0200

    groups table without actions, heroku api,

---
+++ src/components/ActionsMenuGroups.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import GroupIcon from '@material-ui/icons/Group'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import EditIcon from '@material-ui/icons/Edit'
+import DeleteIcon from '@material-ui/icons/Delete'
+import CreditCardIcon from '@material-ui/icons/CreditCard'
+import axios from 'axios'
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+))
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem)
+
+export default function ActionsMenuGroups(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleDelete = (event) => {
+    axios.delete('http://localhost:8010/proxy/groups' + props.id._id)
+    window.location.reload()
+    console.log('buhahah')
+    return false
+  } 
+
+  return (
+    <div style={{ textAlign: 'center'}}>
+      <Button style={{ width: '100%'}}
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+       <MoreVertIcon />
+      </Button>
+      <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem>
+          <ListItemIcon>
+            <EditIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Edytuj dane" />
+        </StyledMenuItem>
+        <StyledMenuItem onClick={handleDelete}>
+          <ListItemIcon>
+            <DeleteIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Usuń" />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <GroupIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Zarządzaj grupami" />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <CreditCardIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Przydziel karnet" />
+        </StyledMenuItem>
+      </StyledMenu>
+    </div>
+  )
+}
